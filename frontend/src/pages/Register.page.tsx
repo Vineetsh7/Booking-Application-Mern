f8de65e50a8e0005547e142680875370b9ff5b53
Vineetsh7 <xyz.meoutlook010@gmail.com>
2024-09-10 11:13:38 +0530

    "Updated Register page with new card component and styling changes"

--- frontend/src/pages/Register.page.tsx
+++ frontend/src/pages/Register.page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 type RegisterFormData = {
   email: string;
@@ -20,95 +21,110 @@ const Register = () => {
     console.log(data);
   });
   return (
-    <div className=" px-40">
-      <form className="flex flex-col gap-5" onSubmit={onSubmit}>
-        <h2 className=" text-3xl font-bold">Create an account</h2>
-        <div className="flex flex-col md:flex-row gap-5">
-          <label htmlFor="" className="text-gray-700 text-sm font-bold flex-1">
-            {" "}
-            First Name
-            <input
-              type="text"
-              className="border rounded w-full py-1 px-2 font-normal"
-              {...register("fullName", { required: "This field is required" })}
-            />
-            {errors.fullName && (
-              <span className="text-red-500">{errors.fullName.message}</span>
-            )}
-          </label>
+    <div className=" px-40 min-h-screen justify-center items-center flex">
+      <form className="flex flex-col gap-5 " onSubmit={onSubmit}>
+        <Card className="bg-slate-700 text-white">
+          <CardHeader>
+            <CardTitle>Create an account</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <h2 className=" text-3xl font-bold">Create an account</h2>
+            <div className="flex flex-col md:flex-row gap-5">
+              <label htmlFor="" className="text-white text-sm font-bold flex-1">
+                {" "}
+                First Name
+                <input
+                  type="text"
+                  className=" bg-zinc-300 text-black border rounded w-full py-1 px-2 font-normal"
+                  {...register("fullName", {
+                    required: "This field is required",
+                  })}
+                />
+                {errors.fullName && (
+                  <span className="text-red-500">
+                    {errors.fullName.message}
+                  </span>
+                )}
+              </label>
 
-          <label htmlFor="" className="text-gray-700 text-sm font-bold flex-1">
-            {" "}
-            Last Name
-            <input
-              type="text"
-              className="border rounded w-full py-1 px-2 font-normal"
-              {...register("lastName", { required: "This field is required" })}
-            />
-            {errors.lastName && (
-              <span className="text-red-500">{errors.lastName.message}</span>
-            )}
-          </label>
-        </div>
+              <label htmlFor="" className="text-white text-sm font-bold flex-1">
+                {" "}
+                Last Name
+                <input
+                  type="text"
+                  className="border rounded w-full py-1 px-2 font-normal"
+                  {...register("lastName", {
+                    required: "This field is required",
+                  })}
+                />
+                {errors.lastName && (
+                  <span className="text-red-500">
+                    {errors.lastName.message}
+                  </span>
+                )}
+              </label>
+            </div>
 
-        <label htmlFor="" className="text-gray-700 text-sm font-bold flex-1">
-          {" "}
-          Email
-          <input
-            type="email"
-            className="border rounded w-full py-1 px-2 font-normal"
-            {...register("email", { required: "Email is required" })}
-          />
-          {errors.email && (
-            <span className="text-red-500">{errors.email.message}</span>
-          )}
-        </label>
+            <label htmlFor="" className="text-white text-sm font-bold flex-1">
+              {" "}
+              Email
+              <input
+                type="email"
+                className="border rounded w-full py-1 px-2 font-normal"
+                {...register("email", { required: "Email is required" })}
+              />
+              {errors.email && (
+                <span className="text-red-500">{errors.email.message}</span>
+              )}
+            </label>
 
-        <label htmlFor="" className="text-gray-700 text-sm font-bold flex-1">
-          {" "}
-          Password
-          <input
-            type="password"
-            className="border rounded w-full py-1 px-2 font-normal"
-            {...register("password", {
-              required: "This field is required",
-              minLength: {
-                value: 6,
-                message: "password must be at least 6 characters",
-              },
-            })}
-          />
-          {errors.password && (
-            <span className="text-red-500">{errors.password.message}</span>
-          )}
-        </label>
+            <label htmlFor="" className="text-white text-sm font-bold flex-1">
+              {" "}
+              Password
+              <input
+                type="password"
+                className="border rounded w-full py-1 px-2 font-normal"
+                {...register("password", {
+                  required: "This field is required",
+                  minLength: {
+                    value: 6,
+                    message: "password must be at least 6 characters",
+                  },
+                })}
+              />
+              {errors.password && (
+                <span className="text-red-500">{errors.password.message}</span>
+              )}
+            </label>
 
-        <label htmlFor="" className="text-gray-700 text-sm font-bold flex-1">
-          {" "}
-          Confirm Password
-          <input
-            type="password"
-            className="border rounded w-full py-1 px-2 font-normal"
-            {...register("confirmPassword", {
-              validate: (val) => {
-                if (!val) {
-                  return "This field is required";
-                } else if (watch("password") !== val) {
-                  return "Your password do not match";
-                }
-              },
-            })}
-          />
-          {errors.confirmPassword && (
-            <span className="text-red-500">
-              {errors.confirmPassword.message}
+            <label htmlFor="" className="text-white text-sm font-bold flex-1">
+              {" "}
+              Confirm Password
+              <input
+                type="password"
+                className="border rounded w-full py-1 px-2 font-normal"
+                {...register("confirmPassword", {
+                  validate: (val) => {
+                    if (!val) {
+                      return "This field is required";
+                    } else if (watch("password") !== val) {
+                      return "Your password do not match";
+                    }
+                  },
+                })}
+              />
+              {errors.confirmPassword && (
+                <span className="text-red-500">
+                  {errors.confirmPassword.message}
+                </span>
+              )}
+            </label>
+
+            <span>
+              <Button type="submit">Register</Button>
             </span>
-          )}
-        </label>
-
-        <span>
-          <Button type="submit">Register</Button>
-        </span>
+          </CardContent>
+        </Card>
       </form>
     </div>
   );
